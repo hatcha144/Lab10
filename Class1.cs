da4f2a2bbcd54f6c8e71de8cdadab4a67df1cd2a
Maria Filice <mfilice@williams-group.com>
2018-10-17 20:21:56 -0400

    Lab 10

---
+++ Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCLab10
+{
+    class Circle
+    {
+        private double radius;
+        
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public double CalculateCircumference()
+        {
+            double pi = Math.PI;
+            double circumference = 2 * pi * radius;
+            return circumference;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            double circumference = CalculateCircumference();
+           
+            return FormatNumber(circumference);
+        }
+        public double CalculateArea()
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+        public string CalculateFormattedArea()
+        {
+            double area = CalculateArea();
+            
+            return FormatNumber(area);
+        }
+        private string FormatNumber(double x)
+        {
+            double cirumference = x;
+            return String.Format("{0:0.##}", x);
+            
+        }
+           
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCLab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius;
+            string restart;
+            List<Circle> circles = new List<Circle>();
+
+            do
+            {
+                do
+                {
+                    try
+                    {
+                        Console.WriteLine("Enter radius: ");
+                        radius = double.Parse(Console.ReadLine());
+                        if (radius > 0)
+
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You must enter positive number");
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("This is not a number");
+                    }
+                }
+                while (true);
+
+
+                Circle circleFinder = new Circle(radius);
+                circles.Add(circleFinder);
+                Console.WriteLine("The area of your number is: " + circleFinder.CalculateFormattedArea());
+
+                Console.WriteLine("The circumference of your number is: " + circleFinder.CalculateFormattedCircumference());
+
+                Console.WriteLine("Do you wish to continue? (Y or N");
+                restart = Console.ReadLine();
+
+
+                if (restart.ToLower() == "n")
+                {
+                    Console.WriteLine("Goodbye! You built " + circles.Count + " cirlce(s)");
+                    
+                }
+
+                
+
+            } while (restart.ToLower() == "y");
+           
+            Console.ReadLine();
+}
+    }
+}
